d08f11ae9aed114231d75e087aeafbeb4a57da11
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-06-07 22:54:16 -0600

    organization project

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-
-const router = express.Router();
-
-router.get('/', (req: Request, res: Response) => {
-  res.send('Ruta de ejemplo en Express y TypeScript');
-});
-
-export default router;
