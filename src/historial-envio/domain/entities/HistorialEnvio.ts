e9afa8a643c9916248f8fea12b8e5e5906388f2e
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-06-13 09:01:34 -0600

    enitie update

---
+++ src/historial-envio/domain/entities/HistorialEnvio.ts
@@ -0,0 +1,12 @@
+export class HistorialEnvio {
+    constructor(
+        public numeroEnvio: string,
+        public fechaEnvio: Date,
+        public direccionOrigen: string,
+        public direccionDestino: string,
+        public pesoPaquete: number,
+        public tipoEnvio: string,
+        public estadoEnvio: string,
+        public costoTotal: number
+    ) {}
+}
