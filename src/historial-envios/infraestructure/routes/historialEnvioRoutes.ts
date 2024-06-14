f9e134c3fe541d1cb875d4dbfe46309d76814500
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-06-13 19:11:07 -0600

    update route

--- src/historial-envios/infraestructure/routes/historialEnvioRoutes.ts
+++ src/historial-envios/infraestructure/routes/historialEnvioRoutes.ts
@@ -1,8 +1,16 @@
 import express from "express";
-
+import { listarHistorialEnvios } from "../../dependencies";
 
 const router = express.Router();
 
-router.get("/historial-envios", listarHistorialEnviosController);
+router.get("/historial-envios", async (req, res) => {
+  try {
+    const historialEnvios = await listarHistorialEnvios.execute();
+    res.json(historialEnvios);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving historical shipments" });
+  }
+});
 
 export default router;
