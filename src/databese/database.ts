915e9b5453b489a5f79c03ec902d60ed08e90d03
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-06-19 14:47:42 -0600

    fix port

--- src/databese/database.ts
+++ src/databese/database.ts
@@ -4,8 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE || '',
-  process.env.DB_USER || '',
+  process.env.DB_DATABASE || 'db_sents',
+  process.env.DB_USER || 'root',
   process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST,
