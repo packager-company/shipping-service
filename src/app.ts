23cdb2a0b1d2626cfaaa20011ede10522b0fe63b
Daniel-Flore2 <danielflores.dev1@gmail.com>
2024-06-30 21:06:15 -0600

    fix port

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import router from './historial-envios/infraestructure/routes/routes';
 import { errorHandler } from './historial-envios/infraestructure/middleware/errorHandler';
-// import router from './shipment_service/routes/routes';
-// import { errorHandler } from './shipment_service/infrastructure/middleware/errorHandler';
-// import sequelize from './database/database';
 import sequelize from './databese/database';
 
 const app = express();
@@ -17,8 +14,8 @@ app.use(errorHandler);
 
 sequelize.sync().then(() => {
   console.log('Database synced');
-  app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+  app.listen(4000, () => {
+    console.log('Server is running on port 4000');
   });
 }).catch((error) => {
   console.error('Unable to sync the database:', error);
